eb1d8b0fe77ac6a740394ee7e09afb6ca2a85422
“Saphron” <saphron.hastie@gmail.com>
2023-09-03 11:45:35 +1000

    Initial play function

--- game.js
+++ game.js
@@ -1,27 +1,47 @@
 "use strict";
 class Player {
-    constructor(name) {
+    constructor(name, mark) {
     }
 }
 var Game;
 (function (Game) {
-    Game.gameBoardArray = ["", "O", "", "", "X", "", "", "", ""];
+    // square = the game element, tile = the display element
+    Game.statusArray = ["", "O", "", "", "X", "", "", "", ""];
+    function play(squareToPlay, marker) {
+        const arrayRef = Number(squareToPlay);
+        Game.statusArray[arrayRef] = marker;
+        Display.refreshBoard();
+    }
+    Game.play = play;
 })(Game || (Game = {}));
 var Display;
 (function (Display) {
     let gameContainer = document.getElementById("gameContainer");
-    function newTile(status) {
+    function newTile(status, ref) {
         const newDisplayTile = document.createElement("div");
         newDisplayTile.innerText = status;
         newDisplayTile.className = "square";
+        newDisplayTile.id = String(ref);
+        newDisplayTile.addEventListener("click", () => {
+            Game.play(newDisplayTile.id, "X");
+        });
         return (newDisplayTile);
     }
     function setupBoard() {
-        Game.gameBoardArray.forEach(tile => {
-            gameContainer.appendChild(newTile(tile));
+        Game.statusArray.forEach(tile => {
+            const tileRef = Game.statusArray.indexOf(tile);
+            gameContainer.appendChild(newTile(tile, tileRef));
         });
     }
     Display.setupBoard = setupBoard;
+    function refreshBoard() {
+        const gameBoard = document.getElementsByClassName("square");
+        Array.from(gameBoard).forEach((tile) => {
+            tile.remove();
+        });
+        setupBoard();
+    }
+    Display.refreshBoard = refreshBoard;
 })(Display || (Display = {}));
 function main() {
     Display.setupBoard();
--- game.ts
+++ game.ts
@@ -1,29 +1,50 @@
 
 class Player {
 
-    constructor(name: string) {
+    constructor(name: string, mark: "X" | "O") {
 
     }
 }
 
 namespace Game {
-    export const gameBoardArray: ("X" | "O" | "")[] = ["", "O", "", "", "X", "", "", "", ""];
+    // square = the game element, tile = the display element
+    export const statusArray: ("X" | "O" | "")[] = ["", "O", "", "", "X", "", "", "", ""];
+
+    export function play(squareToPlay: string, marker: "X" | "O") {
+        const arrayRef = Number(squareToPlay);
+        Game.statusArray[arrayRef] = marker;
+        Display.refreshBoard();
+    }
 
 }
 
 namespace Display {
     let gameContainer = document.getElementById("gameContainer") as HTMLDivElement;
-    function newTile(status: string) {
+    function newTile(status: string, ref: number) {
         const newDisplayTile = document.createElement("div");
         newDisplayTile.innerText = status;
         newDisplayTile.className = "square";
+        newDisplayTile.id = String(ref)
+        newDisplayTile.addEventListener("click", () => {
+            Game.play(newDisplayTile.id, "X")
+        })
         return(newDisplayTile)
     }
 
     export function setupBoard() {
-        Game.gameBoardArray.forEach(tile => {
-            gameContainer.appendChild(newTile(tile))
-    });}
+        Game.statusArray.forEach(tile => {
+            const tileRef = Game.statusArray.indexOf(tile);
+            gameContainer.appendChild(newTile(tile, tileRef))
+    })}
+
+    export function refreshBoard() {
+        const gameBoard = document.getElementsByClassName("square");
+        
+        Array.from(gameBoard).forEach((tile) => {
+            tile.remove();
+        })
+        setupBoard();
+    }
 }
 
 function main() {
